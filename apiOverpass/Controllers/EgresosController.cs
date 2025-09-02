5cf0cfafea1646c7b715e2ab284c2af0c5c4e195
Luis Zelph Moreno Ruíz <luiszelph.morenoruiz@gmail.com>
2025-09-02 17:44:05 -0600

    Se agregaron los metodos para eliminar (baja logica), leer un registro, agregar un registro en el controlador de egresos

--- apiOverpass/Controllers/EgresosController.cs
+++ apiOverpass/Controllers/EgresosController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using Microsoft.EntityFrameworkCore;
 using apiOverpass.Models;
+using Microsoft.Data.SqlClient;
 
 namespace apiOverpass.Controllers
 {
@@ -17,6 +18,11 @@ namespace apiOverpass.Controllers
             _baseDatos = baseDatos;
         }
 
+        /// <summary>
+        /// (2.1) Traer el listado de todos los Egresos
+        /// </summary>
+        /// <returns></returns>
+
         [HttpGet]
         [Route("ListaEgresos")]
         public async Task<ActionResult> ListaEgresos()
@@ -25,5 +31,105 @@ namespace apiOverpass.Controllers
             return Ok(listaEgresos);
         }
 
+        /// <summary>
+        /// (2.2) Traer un Egreso
+        /// </summary>
+        /// <param name="egresoId"></param>
+        /// <returns></returns>
+
+        [HttpGet]
+        [Route("{egresoId:int}", Name = "ObtenerEgreso")]
+        public async Task<ActionResult<TablaEgreso>> ObtenerEgreso(int egresoId)
+        {
+            try
+            {
+                var egreso = await _baseDatos.TablaEgresos.FirstOrDefaultAsync(x => x.EgresoId == egresoId);
+
+                if (egreso == null)
+                {
+                    return NotFound(new { mensaje = "Egreso no encontrado" });
+                }
+
+                return Ok(egreso);
+            }
+            catch (Exception ex)
+            {
+                //return StatusCode(500, new { mensaje = "Ocurrió un error, intente nuevamente" });
+                return StatusCode(500, new { mensaje = ex });
+            }
+        }
+
+        /// <summary>
+        /// (2.3) Metodo para insertar nuevo Egreso
+        /// </summary>
+        /// <param name="tablaEgreso"></param>
+        /// <returns></returns>
+
+        [HttpPost]
+        [Route("InsertarEgreso")]
+        public async Task<CreatedAtRouteResult> InsertarEgreso(TablaEgreso tablaEgreso)
+        {
+            _baseDatos.TablaEgresos.Add(tablaEgreso);
+            await _baseDatos.SaveChangesAsync();
+            return CreatedAtRoute("ObtenerEgreso", new {egresoId = tablaEgreso.EgresoId}, tablaEgreso);
+        }
+
+        /// <summary>
+        /// (2.4) Traer un Egreso
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpPut]
+        [Route("{egresoId:int}", Name = "ActualizarEgreso")]
+        public async Task<ActionResult> ActualizarEgreso(int egresoId, TablaEgreso tablaEgreso)
+        {
+            try
+            {
+                var existeEgreso = await _baseDatos.TablaEgresos.AnyAsync(x => x.EgresoId == egresoId);
+
+                if (!existeEgreso)
+                {
+                    return NotFound(new { mensaje = "Egreso no encontrado" });
+                }
+
+                _baseDatos.Update(tablaEgreso);
+                await _baseDatos.SaveChangesAsync();
+                return Ok("Registro actualizado exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensaje = ex });
+            }
+
+        }
+
+        /// <summary>
+        /// (2.5) Realizar baja lógica de un registro de Egreso
+        /// </summary>
+        /// <param name="egresoId"></param>
+        /// <returns></returns>
+        /// 
+
+        [HttpDelete]
+        [Route("{egresoId:int}", Name = "EliminarEgreso")]
+        public async Task<ActionResult> EliminarEgreso(int egresoId)
+        {
+            try
+            {
+                var filasActualizadas = await _baseDatos.TablaEgresos
+                    .Where(x => x.EgresoId == egresoId)
+                    .ExecuteUpdateAsync(setters => setters
+                        .SetProperty(e => e.Uso, 0));
+               if (filasActualizadas == 0)
+                {
+                    return NotFound(new { mensaje = "Egreso no encontrado" });
+                }
+               return Ok("Egreso eliminado exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensaje = ex });
+            }
+        }
     }
 }
